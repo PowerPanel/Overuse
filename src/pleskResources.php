c766bdd192908b66ce4001858b5d6e3e9599fa10
Peter Notenboom <PeterNotenboom@users.noreply.github.com>
2017-07-28 15:04:35 +0200

    Version 1.2
    Now supports traffic statistics more accurately.

--- src/pleskResources.php
+++ src/pleskResources.php
@@ -3,7 +3,7 @@
  * @author PowerPanel
  * @copyright 2016 PowerPanel BV
  * @since  18-8-2016
- * @version 1.1
+ * @version 1.2
  * Filename:    resourcescriptPlesk.php
  * Based on : https://github.com/plesk/api-examples/tree/master/php
  * Description: class for getting stats back from plesk
@@ -33,61 +33,61 @@ $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 class PleskStats{
 
-    private $_host;
-    private $_port;
-    private $_protocol;
-    private $_login;
-    private $_password;
-    private $_secretKey;
+	private $_host;
+	private $_port;
+	private $_protocol;
+	private $_login;
+	private $_password;
+	private $_secretKey;
 
-    private $domainnames;
+	private $domainnames;
 
-    private function setDomainnames($value){
-        $this->domainnames = $value;
-    }
+	private function setDomainnames($value){
+		$this->domainnames = $value;
+	}
 
-    private function getDomainnames(){
-        return $this->domainnames;
-    }
+	private function getDomainnames(){
+		return $this->domainnames;
+	}
 
-    /**
-     * Create client
-     *
-     * @param string $host
-     * @param int $port
-     * @param string $protocol
-     */
-    public function __construct($host, $port = 8443, $protocol = 'https')
-    {
-        $this->_host = $host;
-        $this->_port = $port;
-        $this->_protocol = $protocol;
-    }
-    /**
-     * Setup credentials for authentication
-     *
-     * @param string $login
-     * @param string $password
-     */
-    public function setCredentials($login, $password)
-    {
-        $this->_login = $login;
-        $this->_password = $password;
-    }
-    /**
-     * Define secret key for alternative authentication
-     *
-     * @param string $secretKey
-     */
-    public function setSecretKey($secretKey)
-    {
-        $this->_secretKey = $secretKey;
-    }
+	/**
+	 * Create client
+	 *
+	 * @param string $host
+	 * @param int $port
+	 * @param string $protocol
+	 */
+	public function __construct($host, $port = 8443, $protocol = 'https')
+	{
+		$this->_host = $host;
+		$this->_port = $port;
+		$this->_protocol = $protocol;
+	}
+	/**
+	 * Setup credentials for authentication
+	 *
+	 * @param string $login
+	 * @param string $password
+	 */
+	public function setCredentials($login, $password)
+	{
+		$this->_login = $login;
+		$this->_password = $password;
+	}
+	/**
+	 * Define secret key for alternative authentication
+	 *
+	 * @param string $secretKey
+	 */
+	public function setSecretKey($secretKey)
+	{
+		$this->_secretKey = $secretKey;
+	}
 
-    /**
-     * Get the statistics from the webserver
-     */
-public function getStats()
+	/**
+	 * Get the statistics from the webserver
+	 */
+	public function getStats()
 	{
 		$resultarray = array();
 		$temparray = array();
@@ -97,10 +97,14 @@ public function getStats()
 				<get>
 				<filter/>
 				<dataset>
-					<hosting/>
 					<stat/>
 				</dataset>
 				</get>
+				<get_traffic>
+					<filter/>
+					<since_date>'. date('Y-m-d', strtotime(' -1 day')) .'</since_date>
+					<to_date>'. date("Y-m-d") .'</to_date>
+				</get_traffic>
 			</webspace>
 		</packet>';
 
@@ -117,70 +121,124 @@ public function getStats()
 			return $returnarray;
 		} else {
 			$resultarray["status"] = 'ok';
-			$result = $array["webspace"]["get"]["result"];
-
 
 			// Result with a single row, create array
-			if (isset($result['status'])) {
-				$result = array(0 => $result);
+			if (isset($array["webspace"]["get"]["result"]['status'])) {
+				$array["webspace"]["get"]["result"] = array($array["webspace"]["get"]["result"]);
 			}
 
-			foreach ($result as $site) {
-				$temparray["domain"] = $site['data']["gen_info"]["name"];
-				$temparray["traffic"] = $site['data']["stat"]["traffic"];
-				$temparray["traffic_prevday"] = $site['data']["stat"]["traffic_prevday"];
-				$temparray["subdomains"] = $site['data']["stat"]["subdom"];
-				$temparray["emailboxen"] = $site['data']["stat"]["box"];
-				$temparray["redirect"] = $site['data']["stat"]["redir"];
-				$temparray["webusers"] = $site['data']["stat"]["wu"];
-				$temparray["mailgroups"] = $site['data']["stat"]["mg"];
-				$temparray["responders"] = $site['data']["stat"]["resp"];
-				$temparray["maillists"] = $site['data']["stat"]["maillists"];
-				$temparray["databases"] = $site['data']["stat"]["db"];
-				$temparray["mssyl_databases"] = $site['data']["stat"]["mssql_db"];
-				$temparray["webapps"] = $site['data']["stat"]["webapps"];
-				$temparray["domains"] = $site['data']["stat"]["domains"];
-				$temparray["sites"] = $site['data']["stat"]["sites"];
-				$temparray["disk_usage"] = $site['data']["gen_info"]["real_size"];
+			foreach ($array["webspace"]["get"]["result"] as $site) {
+
+				$temparray["id"] = $site["id"];
+				$temparray["domain"] = $site["data"]["gen_info"]["name"];
+				//Fallback
+				$temparray["traffic"] = $site["data"]["stat"]["traffic"];
+				$temparray["traffic_prevday"] = $site["data"]["stat"]["traffic_prevday"];
+				//End fallback
+				$temparray["subdomains"] = $site["data"]["stat"]["subdom"];
+				$temparray["emailboxen"] = $site["data"]["stat"]["box"];
+				$temparray["redirect"] = $site["data"]["stat"]["redir"];
+				$temparray["webusers"] = $site["data"]["stat"]["wu"];
+				$temparray["mailgroups"] = $site["data"]["stat"]["mg"];
+				$temparray["responders"] = $site["data"]["stat"]["resp"];
+				$temparray["maillists"] = $site["data"]["stat"]["maillists"];
+				$temparray["databases"] = $site["data"]["stat"]["db"];
+				$temparray["mssyl_databases"] = $site["data"]["stat"]["mssql_db"];
+				$temparray["webapps"] = $site["data"]["stat"]["webapps"];
+				$temparray["domains"] = $site["data"]["stat"]["domains"];
+				$temparray["sites"] = $site["data"]["stat"]["sites"];
+				$temparray["disk_usage"] = $site["data"]["gen_info"]["real_size"];
 				$temparray["log_usage"] = '';
 				$temparray["quota"] = '';
 				$temparray["ssl"] = '';
 				$temparray["cgi"] = '';
 				$temparray["php"] = '';
-				$resultarray["result"][] = $temparray;
+				$resultarray["result"][$site["id"]] = $temparray;
+
 			}
-			return json_encode($resultarray);
+
+			// Result with a single row, create array
+			if (isset($array["webspace"]["get_traffic"]["result"]['status'])) {
+				$array["webspace"]["get_traffic"]["result"] = array($array["get_traffic"]["result"]);
+			}
+			foreach ($array["webspace"]["get_traffic"]["result"] as $site_traffic) {
+				if (isset($resultarray["result"][$site_traffic['id']]) && isset($site_traffic["status"]) && $site_traffic["status"] == 'ok') {
+
+					// Result with a single row
+					if(isset($site_traffic['traffic']['date'])) {
+						if($site_traffic['traffic']['date'] == date("Y-m-d")) {
+							//Today
+							//We add an empty 'yesterday'
+							$site_traffic['traffic'] = array(
+								array(
+									'date' => date('Y-m-d', strtotime(' -1 day')),
+									'http_in' => 0,
+									'http_out' => 0,
+									'ftp_in' => 0,
+									'ftp_out' => 0,
+									'smtp_in' => 0,
+									'smtp_out' => 0,
+									'pop3_imap_in' => 0,
+									'pop3_imap_out' => 0
+								),
+								//and add the 'today':
+								$site_traffic['traffic']
+							);
+						}
+					}
+
+					if(isset($site_traffic['traffic'][0]) && isset($site_traffic['traffic'][1])) {
+
+						unset($site_traffic['traffic'][0]['date']); //prev-day
+						unset($site_traffic['traffic'][1]['date']); //today
+
+						$traffic_prevday = array_sum($site_traffic['traffic'][0]);
+						$traffic = array_sum($site_traffic['traffic'][1]);
+
+						$resultarray["result"][ $site_traffic["id"] ]["traffic"] = $traffic;
+						$resultarray["result"][ $site_traffic["id"] ]["traffic_data"] = $site_traffic['traffic'][1];
+						$resultarray["result"][ $site_traffic["id"] ]["traffic_prevday"] = $traffic_prevday;
+						$resultarray["result"][ $site_traffic["id"] ]["traffic_prevday_data"] = $site_traffic['traffic'][0];
+					}
+					else {
+						//Don't do anything. No valid result(s) we can calculate with
+					}
+				}
+			}
+			//We reset the keys to 0 -> 100 for a better json
+			$resultarray["result"] = array_values($resultarray["result"]);
+
+			return $resultarray;
 		}
 	}
 
-    private function curlRequest($data){
-        $request = $data;
+	private function curlRequest($request){
 
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, "$this->_protocol://$this->_host:$this->_port/enterprise/control/agent.php");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_getHeaders());
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
-        $result = curl_exec($curl);
-        curl_close($curl);
-        return $result;
-    }
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, "$this->_protocol://$this->_host:$this->_port/enterprise/control/agent.php");
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $this->_getHeaders());
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $request);
+		$result = curl_exec($curl);
+		curl_close($curl);
+		return $result;
+	}
 
-    private function _getHeaders()
-    {
-        $headers = array(
-            "Content-Type: text/xml",
-            "HTTP_PRETTY_PRINT: TRUE",
-        );
-        if ($this->_secretKey) {
-            $headers[] = "KEY: $this->_secretKey";
-        } else {
-            $headers[] = "HTTP_AUTH_LOGIN: $this->_login";
-            $headers[] = "HTTP_AUTH_PASSWD: $this->_password";
-        }
-        return $headers;
-    }
+	private function _getHeaders()
+	{
+		$headers = array(
+			"Content-Type: text/xml",
+			"HTTP_PRETTY_PRINT: TRUE",
+		);
+		if ($this->_secretKey) {
+			$headers[] = "KEY: $this->_secretKey";
+		} else {
+			$headers[] = "HTTP_AUTH_LOGIN: $this->_login";
+			$headers[] = "HTTP_AUTH_PASSWD: $this->_password";
+		}
+		return $headers;
+	}
 }
